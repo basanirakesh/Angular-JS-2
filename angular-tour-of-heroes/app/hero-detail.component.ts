b3bcfcff812f42b44928a21777ca50c6972583ae
Basani Rakesh <rakesh.basani@adp.com>
2017-06-08 14:48:11 +0530

    Adding save button

--- angular-tour-of-heroes/app/hero-detail.component.ts
+++ angular-tour-of-heroes/app/hero-detail.component.ts
@@ -31,4 +31,9 @@ export class HeroDetailComponent implements OnInit{
     private route: ActivatedRoute,
     private location: Location
   ) {}
+  
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
 }
